61a178941eca4a26700f086f971ffae8890d85fe
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-30 02:56:16 +0200

    Refatorado o codigo do easter egg

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,12 +1,13 @@
 (() => {
   const $separators = document.querySelectorAll('.separator-dino');
   let counter = 0;
+  const goalShowKey = $separators.length;
 
   $separators.forEach(($separator) => {
     $separator.addEventListener('click', function() {
       !this.classList.contains('-clicked') && counter++;
       this.classList.add('-clicked');
-      counter === 4 && showKey();
+      counter === goalShowKey && showKey();
     });
   });
 
